c78a0ecbb5af5ea7f9387dc6e27290fe101c1e34
Toster111 <mitsuatov08@gmail.com>
2026-03-22 18:10:24 +0300

    'getline' has been replaced with 'get', and bracket counters have been added

--- main.cpp
+++ main.cpp
@@ -3,28 +3,49 @@
 
 // AND OR NOT
 
-#include <iostream>
+#include <cstring>
 
 int main() {
-    std::istream& in = std::cin;
+    std::istream& in = std::cin; // Чтобы было удобнее работать с потоком
 
     const int BUFFER_SIZE = 1024;
-    char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE]; // Буфер для чтения потока
     char symbol{};
+    unsigned int len_array{0};
+
+    unsigned int staples_l = 0;
+    unsigned int staples_r = 0;
 
     // Читаем первый символ
     in.get(symbol);
 
     // Пока не встретили перевод строки и поток не закончился
     while (symbol != '\n' && !in.eof()) {
-		in.unget();
-        // Читаем строку до следующего перевода строки
-        in.getline(buffer, BUFFER_SIZE);
-        std::cout << buffer;
+        ++len_array;
+        buffer[len_array] = symbol;
+        if (symbol == '(') {
+            ++staples_l;
+        }
 
-        // Читаем следующий символ для проверки
+        if (symbol == ')') {
+            ++staples_r;
+        }
+
         in.get(symbol);
     }
+
+	unsigned int counter = 0;
+
+	const int NEW_BUFFER_SIZE = 1024;
+    char new_buffer[NEW_BUFFER_SIZE];
+
+	char * token = strtok(buffer, ")");
+
+	//while (token != nullptr)
+
+
+
+
 
     return 0;
 }
